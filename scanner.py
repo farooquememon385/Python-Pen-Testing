5ce498f54603fb96d1e3dc830ec543f77706f398
farooquememon385 <farooquememon385@gmail.com>
2023-10-12 21:52:04 +0500

    added code to scan SYN ACK scan

--- scanner.py
+++ scanner.py
@@ -1,1 +1,16 @@
 #!usr/bin/python3
+# pip install python-nmap
+import nmap
+scanner = nmap.PortScanner()
+print("Welcome to simple nmap automation tool")
+ip_addr = input("Enter the IP address to scan: ")
+
+resp = input("""\nPlease enter the type of scan you want to run
+             1)SYN ACK Scan
+             2)UDP Scan
+             3)Comprehensive Scan\n:""")
+
+if resp == '1':
+    print()
+    scanner.scan(ip_addr,'1-1024','-v -sS')
+    print(scanner.scaninfo())
