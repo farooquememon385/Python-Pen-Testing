8081b024411098c82ef5c9a188d35284187eb71f
farooquememon385 <farooquememon385@gmail.com>
2023-10-12 22:08:41 +0500

    added functionality for UDP scan and Comprehensive scqn

--- scanner.py
+++ scanner.py
@@ -11,6 +11,28 @@ resp = input("""\nPlease enter the type of scan you want to run
              3)Comprehensive Scan\n:""")
 
 if resp == '1':
-    print()
+    print('Running SYN ACK Scan IP:',ip_addr)
+    # for tcp
     scanner.scan(ip_addr,'1-1024','-v -sS')
     print(scanner.scaninfo())
+    print('IP status:',scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open ports:", scanner[ip_addr])
+elif resp == '2':
+    print('Running UDP Scan IP:',ip_addr)
+    scanner.scan(ip_addr,'1-1024','-v -sU')
+    print(scanner.scaninfo())
+    # for UDP
+    print('IP status:',scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open ports:", scanner[ip_addr])
+elif resp == '3':
+    print('Running Comprehensive Scan IP:',ip_addr)
+    scanner.scan(ip_addr,'1-1024','-v -sS -sV -sC -A -O')
+    print(scanner.scaninfo())
+    print('IP status:',scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open ports:", scanner[ip_addr])
+else:
+    print('Please enter a valid option')
+    exit()
